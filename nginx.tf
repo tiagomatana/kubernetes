779452beeb51058ebea79e92d07fdc04fa1f9cd3
Tiago Matana <tiago.matana@meta.com.br>
2019-07-18 11:56:37 -0300

    First commit

---
+++ nginx.tf
@@ -0,0 +1,59 @@
+
+resource "kubernetes_horizontal_pod_autoscaler" "example" {
+  metadata {
+    name = "terraform-example"
+  }
+  spec {
+    max_replicas = 10
+    min_replicas = 3
+    scale_target_ref {
+      kind = "ReplicationController"
+      name = "example"
+    }
+  }
+}
+
+resource "kubernetes_deployment" "example" {
+  metadata {
+    name = "terraform-example"
+    labels = {
+      test = "MyExampleApp"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        test = "MyExampleApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+          
+          resources {
+            limits {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
